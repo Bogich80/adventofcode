f149ebac0ddb13f9aaa2a98800d99898d421bb70
Roland <bogich80@gmail.com>
2025-11-11 22:09:18 +0100

    Add logic to calculate basement entry position and clean up pom.xml

--- src/main/java/hu/bogich/adventofcode/AdventofcodeApplication.java
+++ src/main/java/hu/bogich/adventofcode/AdventofcodeApplication.java
@@ -9,18 +9,21 @@ import java.io.IOException;
 
 @SpringBootApplication
 public class AdventofcodeApplication {
-
     public static void main(String[] args) throws IOException {
 
         SpringApplication.run(AdventofcodeApplication.class, args);
         FileReader fr = new FileReader("src/main/resources/input_day1.txt");
         System.out.println("Final floor: " + getFloor(fr));
         fr.close();
+        fr = new FileReader("src/main/resources/input_day1.txt");
+        System.out.println("Basement position: " + getBasementPosition(fr));
+        fr.close();
     }
 
     private static int getFloor(FileReader fr) throws IOException {
         int count;
         int floor = 0;
+
         while ((count = fr.read()) != -1) {
             if ((char) count == '(') {
                 floor++;
@@ -32,12 +35,21 @@ public class AdventofcodeApplication {
     }
 
     private static int getBasementPosition(FileReader fr) throws IOException {
-    int count;
-    int floor = 0;
-    while ((count = fr.read()) != -1) {
-        if((char) count == '('){
-            return floor;
+        int count;
+        int floor = 0;
+        int pos = 1;
+        while ((count = fr.read()) != -1) {
+            if ((char) count == '(') {
+                floor++;
+            } else if ((char) count == ')') {
+                floor--;
+            }
+
+            if (floor == -1) {
+                return pos;
+            }
+            pos++;
         }
-    }
+        return -1;
     }
 }

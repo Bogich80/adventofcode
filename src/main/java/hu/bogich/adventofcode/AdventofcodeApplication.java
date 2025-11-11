17d80ec4ec5a52eea4421e286a6ddba47713b579
Bogich80 <bogich80@gmail.com>
2025-11-11 23:47:37 +0100

    Add Advent of Code 2015 Day 2 solution

--- src/main/java/hu/bogich/adventofcode/AdventofcodeApplication.java
+++ src/main/java/hu/bogich/adventofcode/AdventofcodeApplication.java
@@ -3,12 +3,15 @@ package hu.bogich.adventofcode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.FileNotFoundException;
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class AdventofcodeApplication {
+    private static int totalPaper = 0;
+
     public static void main(String[] args) throws IOException {
 
         SpringApplication.run(AdventofcodeApplication.class, args);
@@ -18,6 +21,13 @@ public class AdventofcodeApplication {
         fr = new FileReader("src/main/resources/input_day1.txt");
         System.out.println("Basement position: " + getBasementPosition(fr));
         fr.close();
+        BufferedReader br = new BufferedReader(new FileReader("src/main/resources/2015_input2.txt"));
+        String sor;
+        while ((sor = br.readLine()) != null) {
+            totalPaper += calcTotalPapers(sor);
+        }
+        br.close();
+        System.out.println("Total paper: " + totalPaper);
     }
 
     private static int getFloor(FileReader fr) throws IOException {
@@ -51,5 +61,12 @@ public class AdventofcodeApplication {
             pos++;
         }
         return -1;
+    }
+
+    public static int calcTotalPapers(String dimensions) {
+        String[] dimensionArray = dimensions.split("x");
+        int[] dimensionNumbers = Arrays.stream(dimensionArray).mapToInt(Integer::parseInt).sorted().toArray();
+        return 2 * (dimensionNumbers[0] * dimensionNumbers[1]) + 2 * (dimensionNumbers[1] * dimensionNumbers[2]) +
+                2 * (dimensionNumbers[0] * dimensionNumbers[2]) + dimensionNumbers[0] * dimensionNumbers[1];
     }
 }

a5020bef45b238f55bc382874ccdfb59333b65e2
Bogich80 <bogich80@gmail.com>
2025-11-13 19:18:43 +0100

    Add Advent of Code 2015 Day 4 skeleton and enhance Day 3 solution
    - Add Day 4 class with `CommandLineRunner` implementation.
    - Extend Day 3 logic to handle robot house visits with a new parameter.

--- src/main/java/hu/bogich/adventofcode/Year2015/runner/Day3.java
+++ src/main/java/hu/bogich/adventofcode/Year2015/runner/Day3.java
@@ -19,31 +19,77 @@ public class Day3 implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        log.info("{} houses receive at least one present.", String.valueOf(countHouses()));
+        log.info("{} houses receive at least one present.", countHouses(false));
+        log.info("{} houses receive at least one present.", countHouses(true));
     }
 
-    public int countHouses() throws IOException {
+    public int countHouses(Boolean withRobot) throws IOException {
         int x = 0;
         int y = 0;
+        int robotX = 0;
+        int robotY = 0;
         String content = fileService.readFileByLine(inputFile);
         Map<String, Integer> houses = new HashMap<>();
         houses.put(createCoordinates(x, y), 1);
         for (int i = 0; i < content.length(); i++) {
+            boolean isRobot = (i + 1) % 2 == 0;
             switch (content.charAt(i)) {
                 case '^':
-                    x++;
+                    if (!withRobot) {
+                        x++;
+                    } else {
+                        if (isRobot) {
+                            robotX++;
+                        } else {
+                            x++;
+                        }
+                    }
                     break;
                 case 'v':
-                    x--;
+                    if (!withRobot) {
+                        x--;
+                    } else {
+                        if (isRobot) {
+                            robotX--;
+                        } else {
+                            x--;
+                        }
+                    }
                     break;
                 case '<':
-                    y--;
+                    if (!withRobot) {
+                        y--;
+                    } else {
+                        if (isRobot) {
+                            robotY--;
+                        } else {
+                            y--;
+                        }
+                    }
                     break;
                 case '>':
-                    y++;
+                    if (!withRobot) {
+                        y++;
+                    } else {
+                        if (isRobot) {
+                            robotY++;
+                        } else {
+                            y++;
+                        }
+                    }
                     break;
             }
-            String coordinates = createCoordinates(x, y);
+            String coordinates = null;
+            if (!withRobot) {
+                coordinates = createCoordinates(x, y);
+            } else {
+                if (isRobot) {
+                    coordinates = createCoordinates(robotX, robotY);
+                } else {
+                    coordinates = createCoordinates(x, y);
+                }
+            }
+
             if (houses.containsKey(coordinates)) {
                 houses.put(coordinates, houses.get(coordinates) + 1);
             } else {
@@ -54,8 +100,7 @@ public class Day3 implements CommandLineRunner {
     }
 
     private String createCoordinates(int x, int y) {
-        return String.valueOf(x) + "," + String.valueOf(y);
+        return x + "," + y;
     }
-
 
 }
